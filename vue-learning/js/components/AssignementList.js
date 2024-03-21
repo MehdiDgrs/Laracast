6ec05d831fb35a463a6800121f98d00a1f27401f
MehdiDgrs <mehdigital@outlook.com>
2024-03-21 12:55:38 +0100

    form handling , array to Set, conditional rendering

--- vue-learning/js/components/AssignementList.js
+++ vue-learning/js/components/AssignementList.js
@@ -1,11 +1,18 @@
 import Assignement from "./Assignement.js";
+
 export default {
     template:
 `<section v-show="assignements.length">
+    <div>
     <h1 class="font-bold mb-5">{{title}}</h1>
+    <span>({{assignements.length}})</span>
+</div>
+<div class="mb-2">
+<span v-on="title === 'in progress' " @click="filterByTag" v-for="tag in tags" class="border-2 rounded mx-1 p-1 mb-2" :class="{'text-blue-500 border-blue-500':  tag == this.selectedTag}"> {{tag}} </span>
+</div>
     <ul class="divide-y border-2">
         <assignement
-        v-for="assignement in assignements"
+        v-for="assignement in filteredAssignements"
         :key="assignement.id"
         :assignement="assignement">
         
@@ -16,10 +23,39 @@ export default {
         assignements: {
             type:Array,
         },
+
         title:{
             type:String,
         }
     },
+
+    data(){
+        return {
+            selectedTag : 'all'
+        }
+    },
+
+    computed: {
+        tags() {
+            return ['all', ... new Set(this.assignements.map(a => a.tags))]
+        },
+
+        filteredAssignements() {
+
+            if ( this.selectedTag == "all") {
+                return this.assignements;
+            }
+
+            return this.assignements.filter(a => a.tags === this.selectedTag)
+        }
+    },
+
+    methods: {
+        filterByTag(e) {
+            this.selectedTag = e.target.innerText;
+        }
+    },
+
     components: {
         Assignement,
     }
--- vue-learning/js/components/Assignements.js
+++ vue-learning/js/components/Assignements.js
@@ -2,25 +2,30 @@ import AssignementList from "./AssignementList.js";
 import FormComponent from "./Form.js"
 export default {
     template: `
+  
 <div class="space-y-6">
  <AssignementList :assignements="filters.completed" title="completed"></AssignementList>
  <AssignementList :assignements="filters.inProgress" title="in progress"></AssignementList>
 <FormComponent @add="add"></FormComponent>
 </div>
+     
      `,
+
     data() {
         return {
             message: 'Hello Vue!',
             assignements: [
-                { name: 'Lire plus', done:false , id:1},
-                { name: 'Tondre pelouse', done:false, id:3},
-                { name: 'Randonnée', done:false, id:2},
+                { name: 'Lire plus', tags:'read', done:false , id:1},
+                { name: 'Tondre pelouse',tags:'gardening', done:false, id:3},
+                { name: 'Randonnée',tags:'sport', done:false, id:2},
+                { name: 'Randonnée',tags:'sport', done:false, id:2},
             ],
             completed: [
 
             ],
         }
     },
+
     methods: {
         checked(e){
             this.assignements.done = !this.assignements.done;
@@ -31,16 +36,19 @@ export default {
         add(newItem){
             this.assignements.push({name :newItem, done:false, id: this.assignements.length + 1  });
             newItem="";
-        }
+        },
     },
+
     computed: {
-        filters(){
+        filters() {
             return {
-                completed:this.assignements.filter(i=>i.done),
-                inProgress:this.assignements.filter(i=>!i.done)
-                }
+                completed: this.assignements.filter(i => i.done),
+                inProgress: this.assignements.filter(i => !i.done),
             }
         },
+
+    },
+
     components: {
         AssignementList,
         FormComponent
